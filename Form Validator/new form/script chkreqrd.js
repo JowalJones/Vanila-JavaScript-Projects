d011910796a4d19662f42735849c561072ce411e
Lukorito <jonesdeelder@gmail.com>
2021-05-02 00:03:57 +0300

    naomi prep

--- Form Validator/new form/script chkreqrd.js
+++ Form Validator/new form/script chkreqrd.js
@@ -20,44 +20,31 @@ function showSuccess(input) {
 }
 
 //*Check email is valid
-//Function changed slightly
-function checkEmail(email){
-    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+function isValidEmail(email) {
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
 }
 
 //Check required fields
-function checkRequired(inputArr){
-    inputArr.forEach(function(input){
+function checkRequired(inputArr) {
+  inputArr.forEach(function (input) {
     // console.log(input.value);
     //start with plain input
 
-    if (input.value.trim() === ""){
+    if (input.value.trim() === '') {
       // console.log(input.id);
 
       // showError(input, `${input.id} is required`); // uncapitalized
       showError(input, `${getFieldName(input)} is required`);
-    } else{
+    } else {
       showSuccess(input);
     }
   });
-
-}
-
-//Check input length
-function checkLength(input, min, max){
-  if (input.value.length < min){
-    showError (input, `${getFieldName(input)} must be at least ${min} characters`);
-  } else if (input.value.length > max) {
-    showError (input, `${getFieldName(input)} must be less than ${max} characters`);
-  }else{
-    showSuccess(input);
-  }
 }
 
 //Get fieldName
 
-function getFieldName(input){
+function getFieldName(input) {
   //return input.id.toUpperCase(); //Whole first word is uppercase
   return input.id.charAt(0).toUpperCase() + input.id.slice(1);
 }
@@ -68,8 +55,4 @@ form.addEventListener('submit', function (e) {
 
   // checkRequired(username) //*could do it this way, but it will require calling checkRequired() on every input. Better way is to use an array
   checkRequired([username, email, password, password2]);
-  checkLength(username, 3, 15);
-  checkLength(password, 6, 25); 
-
-
 });
